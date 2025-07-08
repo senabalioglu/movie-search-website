ae8dc75b165894a6280a2446998520e3853491fc
senabalioglu <senabalioglu372@gmail.com>
2025-07-08 19:51:02 +0300

    react router
    react router paketi kuruldu ve basit sayfa geçişi kontrolü yapıldı.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'home',
+        element: <HomePage />,
+      },
+      {
+        path: 'details',
+        element: <DetailPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,4 @@
+
+export default function DetailPage() {
+  return <h1>Detay burası</h1>;
+}
