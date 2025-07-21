dd66d5d498658e75aec86b8ee0d11cd761721398
senabalioglu <senabalioglu372@gmail.com>
2025-07-21 17:19:38 +0300

    header slider cards design
    headersa listelenecek olan kartların tasarımları tamamlandı

--- src/components/HeaderSlider/HeaderSlider.jsx
+++ src/components/HeaderSlider/HeaderSlider.jsx
@@ -1,13 +1,62 @@
-import '../HeaderSlider/HeaderSlider.css'
+import "../HeaderSlider/HeaderSlider.css";
+import React, { useEffect, useState } from "react";
+import { FaStar } from "react-icons/fa6";
 
-function HeaderSlider(){
-    return(
-        <>
-        <div className="slider-container" >
-            <h1 style={{color: 'white'}} >Header Slider</h1>
-        </div>
-        </>
+function HeaderSlider() {
+  const [headerData, setHeaderData] = useState([]);
+  const VITE_TMDB_KEY = "4809431e976e960f71c692b27469b8d2";
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${VITE_TMDB_KEY}`
     )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Film bulunamadı!");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setHeaderData(data.results);
+      })
+      .catch((error) => {
+        return <p> Hata: {error} </p>;
+      });
+  }, []);
+
+  return (
+    <>
+      {headerData?.map((hd, index) => (
+        <>
+          <div className="slider-container">
+            <div>
+              <img
+                className="header-img"
+                src={`https://image.tmdb.org/t/p/original${hd.backdrop_path}`}
+              />
+            </div>
+            <div>
+              <h2 key={index} style={{ color: "white" }}>
+                {hd.title}
+              </h2>
+              <p>
+                {hd.overview.length < 150
+                  ? hd.overview
+                  : hd.overview.slice(0, 150) + "..."}
+              </p>
+              <p>
+                {" "}
+                {hd.vote_average.toFixed(1)}/10{" "}
+                {<FaStar style={{ color: "yellow" }} />}{" "}
+              </p>
+            </div>
+          </div>
+        </>
+      ))}
+    </>
+  );
 }
+
+//{title.length < 25 ? title : title.slice(0, 20) + "..."}
 
 export default HeaderSlider;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,37 +2,12 @@ import React, { useEffect, useState } from "react";
 import HeaderSlider from "../components/HeaderSlider/HeaderSlider";
 
 function HomePage() {
-  const [headerData, setHeaderData] = useState([]);
-  const VITE_TMDB_KEY = "4809431e976e960f71c692b27469b8d2";
-
-  useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/trending/movie/day?api_key=${VITE_TMDB_KEY}`
-    )
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Film bulunamadı!");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setHeaderData(data.results);
-      })
-      .catch((error) => {
-        return <p> Hata: {error} </p>;
-      });
-  }, []);
+  
 
   return (
     <div>
       <HeaderSlider />
-      <div>
-        {headerData?.map((hd, index) => (
-          <li key={index} style={{ color: "white" }}>
-            {hd.title}
-          </li>
-        ))}
-      </div>
+      
     </div>
   );
 }
