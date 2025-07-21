3ebb87f7eff0ff1ef9a79c21826b01e3cf5a1986
senabalioglu <senabalioglu372@gmail.com>
2025-07-21 16:58:51 +0300

    fetched daily trend movies
    günlük trend filimlerin verisi fetch edildi ve basitçe listelendi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderSlider from "../components/HeaderSlider/HeaderSlider";
 
 function HomePage() {
+  const [headerData, setHeaderData] = useState([]);
+  const VITE_TMDB_KEY = "4809431e976e960f71c692b27469b8d2";
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${VITE_TMDB_KEY}`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Film bulunamadı!");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setHeaderData(data.results);
+      })
+      .catch((error) => {
+        return <p> Hata: {error} </p>;
+      });
+  }, []);
+
   return (
     <div>
       <HeaderSlider />
-      <h1>Hoşgeldiniz! Burada sabit içerikler olabilir.</h1>
-      {/* Dilersen başka sabit içerikler ekleyebilirsin */}
+      <div>
+        {headerData?.map((hd, index) => (
+          <li key={index} style={{ color: "white" }}>
+            {hd.title}
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
