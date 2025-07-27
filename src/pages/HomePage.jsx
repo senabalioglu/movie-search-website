8619f99f33c354d420099fa3a66dc9843c031818
Sena Balioğlu <127862385+senabalioglu@users.noreply.github.com>
2025-07-27 22:56:49 +0300

    header image
    The header slider image has been enlarged.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -30,17 +30,17 @@ function HomePage() {
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${VITE_TMDB_KEY}`)
-    .then((response) => {
-      if(!response.ok){
-        console.log("Hata");
-      }
-      return response.json();
-    }).then((data) => {
-      setPopularData(data.results);
-    }).catch((err) => (
-      <h1>Error {err} </h1>
-    ));
-  })
+      .then((response) => {
+        if (!response.ok) {
+          console.log("Hata");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPopularData(data.results);
+      })
+      .catch((err) => <h1>Error {err} </h1>);
+  });
 
   const goToDetail = (top) => {
     navigate(`/details/${top.id}`);
@@ -53,31 +53,35 @@ function HomePage() {
 
       <div className="top-rated-container">
         {topRatedData.map((top) => (
-          <ScrollableSection 
-          key={top.id}
-          onClickFunc={() => goToDetail(top)}
-          compKey={top.id}
-          posterPath={top.poster_path}
-          formattedTitle={ top.title.length < 25 ? top.title : top.title.slice(0, 20) + "..." }
-          date={ top.release_date?.split("-")[0] }
+          <ScrollableSection
+            key={top.id}
+            onClickFunc={() => goToDetail(top)}
+            compKey={top.id}
+            posterPath={top.poster_path}
+            formattedTitle={
+              top.title.length < 25 ? top.title : top.title.slice(0, 20) + "..."
+            }
+            date={top.release_date?.split("-")[0]}
           />
         ))}
       </div>
       <h1 style={{ margin: 10 }}>Popular</h1>
 
       <div className="top-rated-container">
-        {
-          popularData.map((popular) => (
-           <ScrollableSection 
-          key={popular.id}
-          onClickFunc={() => goToDetail(popular)}
-          compKey={popular.id}
-          posterPath={popular.poster_path}
-          formattedTitle={ popular.title.length < 25 ? popular.title : popular.title.slice(0, 20) + "..." }
-          date={ popular.release_date?.split("-")[0] }
+        {popularData.map((popular) => (
+          <ScrollableSection
+            key={popular.id}
+            onClickFunc={() => goToDetail(popular)}
+            compKey={popular.id}
+            posterPath={popular.poster_path}
+            formattedTitle={
+              popular.title.length < 25
+                ? popular.title
+                : popular.title.slice(0, 20) + "..."
+            }
+            date={popular.release_date?.split("-")[0]}
           />
-          ))
-        }
+        ))}
       </div>
     </div>
   );
